ed77b1beda81d2bb35fd427b76bbb9923bc15a8d
Iaroslav Plaksin <Plak.I.A@yandex.ru>
2024-12-09 06:58:34 +0300

    fix (export): see description Большое исправление багов экспорта: установке нехэширования скачиваемых файлов, в ссылках использованы идентификаторы короткой ссылки, исправление кирилицы в JSON, другие мелкие исправления

--- pastehub/export/urls.py
+++ pastehub/export/urls.py
@@ -6,7 +6,7 @@ from export.views import export_docx, export_json, export_source
 app_name = "export"
 
 urlpatterns = [
-    path("source/<str:id_paste>", export_source, name="source"),
-    path("json/<str:id_paste>", export_json, name="json"),
-    path("docx/<str:id_paste>", export_docx, name="docx"),
+    path("<str:short_link>/source", export_source, name="source"),
+    path("<str:short_link>/json", export_json, name="json"),
+    path("<str:short_link>/docx", export_docx, name="docx"),
 ]
