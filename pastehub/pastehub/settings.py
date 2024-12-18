0be449ca9b06a850d92fb93d2755490cc0480488
Iaroslav Plaksin <Plak.I.A@yandex.ru>
2024-12-18 23:24:54 +0300

    chore: update settings.py

--- pastehub/pastehub/settings.py
+++ pastehub/pastehub/settings.py
@@ -124,13 +124,16 @@ LOCALE_PATHS = [
     BASE_DIR / "locale",
 ]
 
-TIME_ZONE = "UTC"
+TIME_ZONE = env("DJANGO_TIME_ZONE", default="UTC")
 
 USE_I18N = True
 
 USE_TZ = True
 
-DATA_UPLOAD_MAX_MEMORY_SIZE = 20 * 1024 * 1024
+DATA_UPLOAD_MAX_MEMORY_SIZE = env.int(
+    "DJANGO_MAX_UPLOAD",
+    default=20 * 1024 * 1024,
+)
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static_dev"]
@@ -146,7 +149,7 @@ LOGOUT_REDIRECT_URL = "/auth/login"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-SHORT_LINK_LENGTH = 8
+SHORT_LINK_LENGTH = env.int("DJANGO_SHORT_LINK_LENGTH", default=8)
 
 DEFAULT_FROM_EMAIL = env.str(
     "DJANGO_DEFAULT_FROM_EMAIL",
@@ -156,3 +159,65 @@ DEFAULT_FROM_EMAIL = env.str(
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 
 EMAIL_FILE_PATH = BASE_DIR / "send_mail"
+
+if not DEBUG:
+    INSTALLED_APPS += ["storages"]
+
+    AWS_ACCESS_KEY_ID = env("S3_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env("S3_SECRET_KEY")
+    AWS_STORAGE_BUCKET_NAME = env("S3_BUCKET_NAME")
+    AWS_S3_REGION_NAME = env("S3_REGION", default="ru-central1")
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_ENDPOINT_URL = env(
+        "S3_ENDPOINT_URL",
+        default="https://storage.yandexcloud.net",
+    )
+
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3.S3Storage",
+            "OPTIONS": {
+                "location": "media",
+            },
+        },
+        "staticfiles": {
+            "BACKEND": "storages.backends.s3.S3Storage",
+            "OPTIONS": {
+                "location": "static",
+            },
+        },
+    }
+
+    MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/media/"
+    STATIC_URL = f"{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/static/"
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": env("DB_NAME"),
+            "USER": env("DB_USER"),
+            "PASSWORD": env("DB_PASSWORD"),
+            "HOST": env("DB_HOST"),
+            "PORT": env("DB_PORT"),
+            "OPTIONS": {
+                "sslmode": "verify-full",
+                "sslrootcert": env(
+                    "DB_CERT_PATH",
+                    default=BASE_DIR / "db_root.crt",
+                ),
+            },
+        },
+    }
+
+    CSRF_TRUSTED_ORIGINS = env.list("DJANGO_CSRF_TRUSTED_ORIGINS", default=[])
+
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = env("EMAIL_HOST", default="postbox.cloud.yandex.net")
+    EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
+    EMAIL_HOST_USER = env("EMAIL_USER", default="API_KEY")
+    EMAIL_PORT = 587
+    DEFAULT_FROM_EMAIL = env.str(
+        "DJANGO_DEFAULT_FROM_EMAIL",
+        default="no-reply@paste-hub.ru",
+    )
+    EMAIL_USE_TLS = True
