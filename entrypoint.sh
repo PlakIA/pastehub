6b3ca6648aa53db105ef658ef6e216d0e644e6b9
Iaroslav Plaksin <Plak.I.A@yandex.ru>
2024-12-20 00:13:20 +0300

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ echo "Static Collecting..."
 python manage.py collectstatic --noinput
 
 echo "Messages Compiling..."
-python manage.py compilemessages --noinput
+python manage.py compilemessages
 
 echo "Start gunicorn"
 exec gunicorn pastehub.wsgi:application --workers=$GUNICORN_WORKERS --bind 0.0.0.0:$PORT
